432cb8d264aa3d1fa304db16389379b6289fe50a
Benwell Mortimer <tom.benwell.mortimer@cgi.com>
2022-11-08 11:32:45 +0000

    Adding basic REST endpoint

--- src/main/java/org/tbm/javaplay/JavaPlayApplication.java
+++ src/main/java/org/tbm/javaplay/JavaPlayApplication.java
@@ -1,7 +1,12 @@
 package org.tbm.javaplay;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JavaPlayApplication {
@@ -9,6 +14,21 @@ public class JavaPlayApplication {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		SpringApplication.run(JavaPlayApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
 	}
 
 }
