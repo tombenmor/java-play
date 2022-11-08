ae1e28a12e492d51ebe5bafa90d56c521d74d96b
Benwell Mortimer <tom.benwell.mortimer@cgi.com>
2022-11-08 11:20:18 +0000

    Initial project commit: basic springboot service generated by start.spring.io

---
+++ src/main/java/org/tbm/javaplay/JavaPlayApplication.java
@@ -0,0 +1,14 @@
+package org.tbm.javaplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPlayApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		SpringApplication.run(JavaPlayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tbm/javaplay/JavaPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tbm.javaplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
